21c6e6221a5f2f2c89f7984c8abff08d08331c47
Hyunsik Choi <hyunsik@apache.org>
2015-11-12 00:00:46 -0800

    Fixed compilation err.

--- src/engine.rs
+++ src/engine.rs
@@ -151,7 +151,7 @@ impl<'a, 'b:'a> ExecutionEngine<'a, 'b> for JitEngine<'a> {
                 MCJMM: ptr::null_mut()
             };
             let size = mem::size_of::<LLVMMCJITCompilerOptions>();
-            let result = engine::LLVMCreateMCJITCompilerForModule(&mut ee, (&*module).into(), &mut options, size as size_t, &mut out);
+            let result = engine::LLVMCreateMCJITCompilerForModule(&mut ee, (&*module).into(), &mut options, size as u64, &mut out);
             if result == 0 {
                 Ok(ee.into())
             } else {
