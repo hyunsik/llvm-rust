f109466e05077668ebaed2c50d43f91937036703
Hyunsik Choi <hyunsik@apache.org>
2015-11-17 12:19:02 -0800

    reformat context.rs.

--- src/context.rs
+++ src/context.rs
@@ -1,6 +1,6 @@
+use cbox::CBox;
 use ffi::prelude::LLVMContextRef;
 use ffi::{core, LLVMContext};
-use cbox::CBox;
 
 /// Contains all the LLVM entities - mainly modules.
 ///
@@ -9,30 +9,36 @@ use cbox::CBox;
 /// preserve pointer safety.
 pub struct Context;
 native_ref!(&Context = LLVMContextRef);
-impl Context {
-    /// Get a reference to the global context.
-    ///
-    /// This is marked as unsafe because this can result in undefined behaviour
-    /// in a multithreaded context if they all use the same context.
-    pub unsafe fn get_global() -> &'static Context {
-        core::LLVMGetGlobalContext().into()
-    }
-    /// Create a new context, which is owned by the callee block.
-    pub fn new() -> CBox<Self> {
-        CBox::new(unsafe { core::LLVMContextCreate() })
-    }
+
+impl Context 
+{
+  /// Get a reference to the global context.
+  ///
+  /// This is marked as unsafe because this can result in undefined behaviour
+  /// in a multithreaded context if they all use the same context.
+  pub unsafe fn get_global() -> &'static Context 
+  {
+    core::LLVMGetGlobalContext().into()
+  }
+  
+  /// Create a new context, which is owned by the callee block.
+  pub fn new() -> CBox<Self> 
+  {
+    CBox::new(unsafe { core::LLVMContextCreate() })
+  }
 }
 dispose!(Context, LLVMContext, core::LLVMContextDispose);
 
 /// Implemented by everything that is owned by a context.
-pub trait GetContext {
-    /// Returns a reference to the context that owns this value.
-    ///
-    /// ```rust
-    /// use llvm::*;
-    /// let context = Context::new();
-    /// let module = Module::new("rowrowfightthapowa", &context.as_semi());
-    /// assert!(context == *module.get_context());
-    /// ```
-    fn get_context(&self) -> &Context;
+pub trait GetContext 
+{
+  /// Returns a reference to the context that owns this value.
+  ///
+  /// ```rust
+  /// use llvm::*;
+  /// let context = Context::new();
+  /// let module = Module::new("rowrowfightthapowa", &context.as_semi());
+  /// assert!(context == *module.get_context());
+  /// ```
+  fn get_context(&self) -> &Context;
 }
